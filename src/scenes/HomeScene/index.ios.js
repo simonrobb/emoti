b37d79cd88e1369ad62736f8f2cf9966ad532818
Simon Robb <simon@Simons-MacBook-Pro.local>
2016-09-21 11:03:01 -0400

    Added carousel arrow controls

--- src/scenes/HomeScene/index.ios.js
+++ src/scenes/HomeScene/index.ios.js
@@ -18,11 +18,13 @@ const packs = [
   {
     name: 'Animals',
     image: require('./assets/animals.jpg'),
+    logoColor: '#476079',
     emojis: AnimalEmojis
   },
   {
     name: 'Travel',
     image: require('./assets/travel.jpg'),
+    logoColor: 'white',
     emojis: FacesEmojis
   }
 ]
@@ -32,7 +34,7 @@ class HomeScene extends Component {
     super(props)
 
     this.state = {
-      currentIndex: 0, 
+      currentPack: packs[0], 
       openingPack: null
     }
     this.openAnimValue = new Animated.Value(0)
@@ -74,6 +76,26 @@ class HomeScene extends Component {
     ).start(() => callback(pack))
   }
 
+  gotoPackByIndex(index) {
+    const pack = packs[index]
+
+    if (!pack) {
+      throw `Invalid pack index {${index}} passed to gotoPackByIndex`
+    }
+
+    // Start the carousel animation
+    this._carousel.goToPage(index)
+
+    // Update the current pack approximately halfway through the animation
+    setTimeout(() => {
+      this.setState({ currentPack: pack })
+    }, 500)
+  }
+
+  getCurrentPackIndex() {
+    return packs.indexOf(this.state.currentPack)
+  }
+
   openPack(pack) {
     const { onPlayPress } = this.props
     onPlayPress(pack)
@@ -81,6 +103,14 @@ class HomeScene extends Component {
 
   handlePageChange(index) {
     this.setState({ currentIndex: index })
+  }
+
+  handlePrevPackPress() { 
+    this.gotoPackByIndex(this.getCurrentPackIndex()-1)
+  }
+
+  handleNextPackPress() {
+    this.gotoPackByIndex(this.getCurrentPackIndex()+1)
   }
 
   handleStartShouldSetResponder(event) {
@@ -104,7 +134,7 @@ class HomeScene extends Component {
     const playButtonPressAnimStyle = {
       transform: [{scale: this.playButtonPressAnimValue.interpolate({
           inputRange: [0, 0.5, 1],
-          outputRange: [1, 0.9, 4]
+          outputRange: [1, 0.8, 4]
         })
       }],
       opacity: this.playButtonPressAnimValue.interpolate({
@@ -141,7 +171,9 @@ class HomeScene extends Component {
         extrapolate: 'clamp'
       })
     }
-    const logoStyles = [styles.logo, logoAnimStyle]
+    const logoStyles = [styles.logo, logoAnimStyle, {
+      tintColor: this.state.currentPack.logoColor
+    }]
 
     // Carousel bar animation styles
     const carouselBarAnimStyle = {
@@ -159,46 +191,76 @@ class HomeScene extends Component {
     }
     const carouselBarStyles = [styles.carouselBar, carouselBarAnimStyle]
 
-    return (
-      <Carousel pageWidth={width} sneak={0} initialPage={this.state.currentIndex} onPageChange={index => this.handlePageChange(index)} style={styles.carousel}>
+    // Arrow styles
+    const currIndex = this.getCurrentPackIndex()
+    const arrowLeftStyles = [styles.arrow, styles.arrowLeft, { opacity: (currIndex <= 0 ? 0 : 1) }]
+    const arrowRightStyles = [styles.arrow, styles.arrowRight, { opacity: (currIndex >= (packs.size-1) ? 0 : 1) }]
+
+    // Carousel element
+    const carouselDimensionsStyle = {
+      width,
+      height
+    }
+    const carouselStyles = [styles.carousel, carouselDimensionsStyle]
+    const carouselEl = <View style={carouselStyles}>
+      <Carousel pageWidth={width} sneak={0} initialPage={this.state.currentIndex} onPageChange={index => this.handlePageChange(index)} ref={ref => this._carousel = ref}>
         {packs.map(pack => {
-          const imageStyles = [styles.image, {
-            width,
-            height
-          }]
+          const imageStyles = [styles.image, carouselDimensionsStyle]
           if (this.state.openingPack === pack.name) {
             imageStyles.push(openAnimStyle)
           }
 
-          return <View style={styles.pack} key={pack.name}>
-            <View style={styles.wrapper}>
-              <Animated.Image style={imageStyles} source={pack.image} />
-              <View style={styles.content}>
-                <Animated.Image source={require('./assets/logo.png')} style={logoStyles}  />
-                <View style={styles.play}>
-                  <Animated.Image source={require('./assets/play.png')} style={playButtonStyles} onStartShouldSetResponder={event => this.handleStartShouldSetResponder(event)} onResponderGrant={event => this.handleResponderGrant(event)} onResponderMove={event => this.handleResponderMove(event)} onResponderRelease={event => this.handleResponderRelease(event, pack)} />
-                </View>
-                <Animated.View style={carouselBarStyles}>
-                  <Text style={styles.packName}>{pack.name}</Text>
-                </Animated.View>
-              </View>
-            </View>
-          </View>
+          return <Animated.Image style={imageStyles} source={pack.image} key={pack.name} />
         })}
       </Carousel>
-    )
+    </View>
+
+    // Overlay element
+    const overlayEl = <View style={styles.pack} key={this.state.currentPack.name}>
+      <View style={styles.wrapper}>
+        <View style={styles.content}>
+          <Animated.Image source={require('./assets/logo.png')} style={logoStyles}  />
+          <View style={styles.play}>
+            <Animated.Image source={require('./assets/play.png')} style={playButtonStyles} onStartShouldSetResponder={event => this.handleStartShouldSetResponder(event)} onResponderGrant={event => this.handleResponderGrant(event)} onResponderMove={event => this.handleResponderMove(event)} onResponderRelease={event => this.handleResponderRelease(event, this.state.currentPack)} />
+          </View>
+          <Animated.View style={carouselBarStyles}>
+            <View>
+              <TouchableWithoutFeedback onPress={() => this.handlePrevPackPress()}>
+                <Image source={require('./assets/arrow.png')} style={arrowLeftStyles} />
+              </TouchableWithoutFeedback>
+            </View>
+            <Text style={styles.packName}>{this.state.currentPack.name}</Text>
+            <View>
+              <TouchableWithoutFeedback onPress={() => this.handleNextPackPress()}>
+                <Image source={require('./assets/arrow.png')} style={arrowRightStyles} />
+              </TouchableWithoutFeedback>
+            </View>
+          </Animated.View>
+        </View>
+      </View>
+    </View>
+
+    // Put it together
+    return <View style={styles.container}>
+      {carouselEl}
+      {overlayEl}
+    </View>
   }
 }
 
 const styles = StyleSheet.create({
-  pack: {
-    flex: 1,
-    zIndex: 1
+  container: {
+    flex: 1
   },
-  image: {
+  pack: {
+    flex: 1
+  },
+  carousel: {
     position: 'absolute',
     top: 0,
-    left: 0,
+    left: 0
+  },
+  image: {
     justifyContent: 'center',
     alignItems: 'center',
     resizeMode: Image.resizeMode.cover
@@ -220,12 +282,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   carouselBar: {
+    flexDirection: 'row',
     width: null,
     height: 44,
     marginBottom: 37,
     backgroundColor: 'rgba(0,0,0,0)'
   },
   packName: {
+    flex: 1,
     color: 'white',
     fontSize: 36,
     fontWeight: 'bold',
@@ -233,7 +297,21 @@ const styles = StyleSheet.create({
     shadowColor: 'black',
     shadowOpacity: 0.1,
     shadowRadius: 0,
-    shadowOffset: { width: 1, height: 2}
+    shadowOffset: { width: 1, height: 2},
+    textAlign: 'center'
+  },
+  arrow: {
+    marginLeft: 20,
+    marginRight: 20,
+    shadowColor: 'black',
+    shadowOpacity: 0.1,
+    shadowRadius: 0,
+    shadowOffset: { width: 1, height: 2},
+    opacity: 0.8
+  },
+  arrowLeft: {
+    transform: [{rotate: '180deg'}],
+    shadowOffset: { width: -1, height: -2},
   }
 });
 
